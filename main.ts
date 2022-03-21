29f8784fdf8581bd54cbea137c18aa1cb4fb0a4a
Mc. Webstar <101481448+McWebstar@users.noreply.github.com>
2022-03-21 21:38:11 +0100

    neuer block

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Hello!")
     music.playMelody("- - - A - B - - ", 120)
 })
+loops.everyInterval(500, function () {
+    led.plot(0, 0)
+})
 basic.forever(function () {
 	
 })
