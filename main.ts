22472ff5f0e479dd0777b4d387aee6034c2049e6
Mc. Webstar <101481448+McWebstar@users.noreply.github.com>
2022-03-21 22:28:21 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("Hello!")
-    music.playMelody("- - - A - B - - ", 120)
 })
 loops.everyInterval(500, function () {
     led.plot(0, 0)
