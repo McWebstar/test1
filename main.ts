85668596e1c91133196caa6ac71be2b55f5ce776
Mc. Webstar <101481448+McWebstar@users.noreply.github.com>
2022-03-22 21:51:29 +0100

    blink

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Hello!")
 })
 loops.everyInterval(500, function () {
-    led.plot(0, 0)
+    led.toggle(0, 0)
 })
 basic.forever(function () {
 	
